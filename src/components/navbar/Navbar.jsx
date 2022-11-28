96396090171704e041a61b105629fc997cc61b12
iamkashifusman <kashif.usman@assistechsol.com>
2022-11-28 18:29:04 +0500

    added navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './navbar.css'
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 const Navbar = () => {
   return (
     <div className='gpt__navbar'>
-      
+      <div className='gpt__navbar-links'>
+        <div className='gpt__navbar-links_logo'>
+            <img src={logo} alt='logo'/>
+        </div>
+        <div className='gpt__navbar-links_container'>
+            <p><a href='#home'>Home</a></p>
+            <p><a href='#wgpt3'>What is GPT?</a></p>
+            <p><a href='#possibility'>Open AI</a></p>
+            <p><a href='#features'>Case Studies</a></p>
+            <p><a href='#blog'>Library</a></p>
+        </div>
+        <div className='gpt__navbar-sign'>
+          <p> Sign Up</p>
+          <button type='button'> Sign Up</button>
+        </div>
+      </div>
     </div>
   )
 }
